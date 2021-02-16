e742d0cc2574f218051804b98c380a40ec1d2b63
Josh98Garcia <46909721+Josh98Garcia@users.noreply.github.com>
2021-02-16 13:42:35 -0600

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import Tflite from 'tflite-react-native';
+import ImagePicker from 'react-native-image-picker';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+import CameraPage from './src/camera.page';
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}
+
+
+
+
